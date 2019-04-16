147a2186486d656ac74e024db01aa178c1b5df1a
Truchisushi <trochezdaniel95@gmail.com>
2019-04-16 10:31:14 +0200

    exercise 2 sum

--- Lab3/OpenMP/exercise_2/src/sum.c
+++ Lab3/OpenMP/exercise_2/src/sum.c
@@ -2,12 +2,47 @@
 
 void omp_sum(double *sum_ret)
 {
-
+	double sum = 0;
+	//double *part_sum = (double*)calloc(omp_get_num_threads(), sizeof(double));
+	
+	#pragma omp parallel
+	{
+	//	int id = omp_get_thread_num();
+		int id, num_threads;
+		id = omp_get_thread_num();
+		num_threads = omp_get_num_threads();
+		for(int i = id*size/num_threads; i < (id+1)*size/num_threads; i++)
+		{
+			sum += x[i];
+		}
+		//part_sum[id] = sum;
+	}	
+	
+	//for(int i = 0; i < omp_get_num_threads(); i++){
+	//	sum += part_sum[i];
+	//}
+	*sum_ret = sum;
 }
 
 void omp_critical_sum(double *sum_ret)
 {
+	double sum = 0;
+	
+	#pragma omp parallel
+	{
+		int id, num_threads;
+		id = omp_get_thread_num();
+		num_threads = omp_get_num_threads();
 
+		
+		for(int i = id; i < size; i+=num_threads)
+		{
+			#pragma omp critical
+			sum += x[i];
+		}	
+	}
+	
+	*sum_ret = sum;
 }
 
 void omp_atomic_sum(double *sum_ret)
@@ -17,7 +52,27 @@ void omp_atomic_sum(double *sum_ret)
 
 void omp_local_sum(double *sum_ret)
 {
+	//double sum = 0;
+	double *part_sum = (double*)calloc(omp_get_num_threads(), sizeof(double));
+	
+	#pragma omp parallel
+	{
+		int num_threads = omp_get_num_threads();
+		int id;
+		id = omp_get_thread_num();
+		for(int i = id*size/num_threads; i < (id+1)*size/num_threads; i++)
+		{
+			part_sum[id] += x[i];
+		}
+		//part_sum[id] = sum;
+	}
 
+	double res_sum = 0;
+
+	for(int i = 0; i < omp_get_num_threads(); i++)
+		res_sum += part_sum[i];
+
+	*sum_ret = res_sum;
 }
 
 void omp_padded_sum(double *sum_ret)
