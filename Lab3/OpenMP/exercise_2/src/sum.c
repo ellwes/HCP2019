4037cd22a81b8fbd567757de6838507dc36f0369
Ellinor Westerberg <ell.westerberg@gmail.com>
2019-04-16 10:08:53 +0200

    added atomic

--- Lab3/OpenMP/exercise_2/src/sum.c
+++ Lab3/OpenMP/exercise_2/src/sum.c
@@ -12,6 +12,16 @@ void omp_critical_sum(double *sum_ret)
 
 void omp_atomic_sum(double *sum_ret)
 {
+  double sum = 0;
+  #pragma omp parallel
+	{
+    for (int i = 0; i < size; i++){
+      #pragma omp atomic
+  		 sum += x[i];
+    }
+	}
+	*sum_ret = sum;
+
 
 }
 
