d3d2b5cb389956ed3b7ccf1bf6f527b12d565e69
Ellinor Westerberg <ell.westerberg@gmail.com>
2019-04-16 16:32:46 +0200

    updated with fastest so far

--- Lab3/OpenMP/exercise_3/src/dft.c
+++ Lab3/OpenMP/exercise_3/src/dft.c
@@ -1,20 +1,26 @@
 #include "dft.h"
+#include <omp.h>
 
 // DFT/IDFT routine
 // idft: 1 direct DFT, -1 inverse IDFT (Inverse DFT)
 int DFT(int idft, double* xr, double* xi, double* Xr_o, double* Xi_o, int N)
-{
-	for (int k = 0 ; k < N; k++) {
-		for (int n = 0; n < N; n++) {
-			// Real part of X[k]
-			Xr_o[k] += xr[n] * cos(n * k * PI2 / N) + idft * xi[n] * sin(n * k * PI2 / N);
-			// Imaginary part of X[k]
-			Xi_o[k] += -idft * xr[n] * sin(n * k * PI2 / N) + xi[n] * cos(n * k * PI2 / N);
-		}
-	}
-		
+{	
+	#pragma omp parallel for 
+	for (int k = 0 ; k < N; k++) {	
+		//double real = Xr_o[k];
+		//double img = Xi_o[k];
+			for (int n = 0; n < N; n++) {
+				// Real part of X[k]
+				Xr_o[k] += xr[n] * cos(n * k * PI2 / N) + idft * xi[n] * sin(n * k * PI2 / N);
+				// Imaginary part of X[k]
+				Xi_o[k] += -idft * xr[n] * sin(n * k * PI2 / N) + xi[n] * cos(n * k * PI2 / N);
+			}
+			//Xr_o[k] = real;
+			//Xi_o[k] = img;
+	}	
 	// normalize if you are doing IDFT
-	if (idft == -1){
+	if (idft == -1) {
+		#pragma omp parallel for
 		for (int n = 0; n < N; n++) {
 			Xr_o[n] /= N;
 			Xi_o[n] /= N;
