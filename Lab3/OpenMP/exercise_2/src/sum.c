f718f28d09947dd7f03d7b6326e89f6a38bde8e6
Ellinor Westerberg <ell.westerberg@gmail.com>
2019-04-16 14:30:24 +0200

    sum updated

--- Lab3/OpenMP/exercise_2/src/sum.c
+++ Lab3/OpenMP/exercise_2/src/sum.c
@@ -5,7 +5,8 @@ void omp_sum(double *sum_ret)
 	double sum = 0; 
 	#pragma omp parallel
 	{
-		for(int = 0; i < size; i++){
+		#pragma omp for
+		for(int i = 0; i < size; i++){
 			sum += x[i];	
 		}	
 	}
@@ -19,7 +20,7 @@ void omp_critical_sum(double *sum_ret)
 	double sum = 0;
 	#pragma omp parallel
 	{
-		
+		#pragma omp for	
 		for(int i = 0; i < size; i++)
 		{
 			#pragma omp critical
@@ -35,6 +36,7 @@ void omp_atomic_sum(double *sum_ret)
   double sum = 0;
   #pragma omp parallel
   {
+    #pragma omp for
     for (int i = 0; i < size; i++){
 	#pragma omp atomic
 	 sum += x[i];
@@ -47,23 +49,20 @@ void omp_atomic_sum(double *sum_ret)
 
 void omp_local_sum(double *sum_ret)
 {
- 	double sum = 0; ii
-	int num_threads = omp_get_num_threads();
-	double *part_sum = (double*)calloc(num_threads, sizeof(double));
+	double *part_sum = (double*)calloc(32, sizeof(double));
 	#pragma omp parallel
 	{
 		int id = omp_get_thread_num();
-		for(int i = id*size/num_threads; i < (id+1)*size/num_threads; i++)
+		#pragma omp for
+		for(int i = 0; i < size; i++)
 		{
 			part_sum[id] += x[i];
 		}
 	}
 
 	double res_sum = 0;
-
-	for(int i = 0; i < omp_get_num_threads(); i++)
+	for(int i = 0; i < 32; i++)
 		res_sum += part_sum[i];
-		printf("SUM: %d\n", res_sum);
 
 	*sum_ret = res_sum;
 }
@@ -71,14 +70,53 @@ void omp_local_sum(double *sum_ret)
 void omp_padded_sum(double *sum_ret)
 {
 
+        double *part_sum = (double*)calloc(64, sizeof(double));
+        #pragma omp parallel
+        {
+                int id = omp_get_thread_num();
+		#pragma omp for
+                for(int i = 0; i < size; i++)
+                {
+                        part_sum[id*3] += x[i];
+                }
+        }
+
+        double res_sum = 0;
+        for(int i = 0; i < 64; i++)
+                res_sum += part_sum[i];
+
+        *sum_ret = res_sum;
 }
 
 void omp_private_sum(double *sum_ret)
 {
 
+	double res_sum = 0;
+	#pragma omp parallel
+        {
+		double sum = 0;
+                #pragma omp for
+                for(int i = 0; i < size; i++)
+                {
+                        sum += x[i];
+                }
+        	res_sum += sum;
+	}
+        *sum_ret = res_sum;
+
 }
 
 void omp_reduction_sum(double *sum_ret)
 {
+	double sum = 0; 
+	#pragma omp parallel
+	{
+		#pragma omp for reduction(+: sum)
+		for (int i = 0; i < size; i++) {
+			sum += x[i];
+		}
+		
+	}	
+	*sum_ret = sum;
 
 }
