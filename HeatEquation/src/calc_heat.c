9163cb577b9f4ff2784a098ea609e945fed180c3
Truchisushi <trochezdaniel95@gmail.com>
2019-05-26 18:30:19 +0200

    Add step functions

--- HeatEquation/src/calc_heat.c
+++ HeatEquation/src/calc_heat.c
@@ -8,6 +8,7 @@ struct Config {
 	double ** matrix_tmp;
 	int matrix_dim[2];
 	int matrix_size;
+		
 
 	/* Communicators */
 	MPI_Comm grid_comm;
@@ -22,6 +23,10 @@ struct Config {
 	/* Local dims and local type*/
 	int local_dims[2];
 	int block;
+
+	/*spatial*/
+	double hx;
+	double hy
 
 	/* Time */	
 	double time;
@@ -57,6 +62,10 @@ void init_solver(const double K, int size, const double heat_start, const double
  	/* Create data array */
 	double m[size][size];
 	double m_tmp[size][size];
+
+	/*spatial time*/
+	config.hx = 1/size;
+	config.hy = 1/size;
 
 	config.matrix = &m; 
 	config.matrix_tmp = &m_tmp;
@@ -96,9 +105,81 @@ void cleanup() {
 
 void calc_heat() {
  
- for(double t = config.time; t > 0; t--) {
+ for(double t = config.time; t > 0; t--) { //Maybe change this to the other way around? (t = 0; t < config.time; t++)
  	/* Broadcast values to close cells */
  	/* Calculate current theta */
+	step();
+	MPI_Barrier();
  }
 
 }
+
+void step() {
+	
+	//	up, down, left, right
+	int dir[4] = {-1, -1, -1, -1};
+	int offset_row[4] = {0, config.local_dims[0], 1, 1}; //row offset start
+	int offset_col[4] = {1, 1, 0, config.local_dims[1]}; //col offset start
+	int rec_row, rec_col;	//receiving row & column pos.	
+
+	//Observe that this might be wrong and more Cart_shifts may be needed.
+	MPI_Cart_shift(config.grid_comm, 0, 1, &dir[0], &dir[1]);	//shift up/down
+	MPI_Cart_shift(config.grid_comm, 1, 1, &dir[2], &dir[3]);	//shift left/right
+	printf("I am %d, up/down is %d/%d. And my left/right is %d/%d", config.grid_rank, dir[0], dir[1], dir[2], dir[3]);
+
+	int i;
+	double send_buffer, recv_buffer;
+	for(i = 0; i < 4; i++){
+		//Get the element to send and put it in buffer:
+		int j;
+		for(j = 0; j < config.local_size-2; j++){
+			//send buffer depending if it's a row or col ghost cell.
+			//send_buffer = i < 2 ? config.matrix[offset_row[i]][offset_col[i]+j] : config.matrix[offset_row[i]+j][offset_col[i]]
+			//set send_buffer and receiving row/col to correct values depending on current row that is being sent.
+			if(i < 2){
+				send_buffer = config.matrix[offset_row[i]][offset_col[i] + j];
+				rec_row = abs(offset_row[i] - 1);
+				rec_col = offset_col[i] + j;
+			}
+			else{
+				send_buffer = config.matrix[offset_row[i] + j][offset_col[i]];
+				rec_row = offset_row[i] + j;
+				rec_col = abs(offset_col[i] - 1);
+			}
+			
+			if(dir[i] != MPI_PROC_NULL){ //send & recv.
+				MPI_Send(&send_buffer, 1, MPI_DOUBLE, dir[i], config.grid_rank, config.grid_comm);
+				MPI_Recv(&recv_buffer, 1, MPI_DOUBLE, dir[i], config.grid_comm, MPI_STATUS_IGNORE);
+			}
+			else{
+				//Process tries to send to something outside of the grid space.
+				// set recv_buffer to send buffer..
+				recv_buffer = send_buffer;
+			}
+			//Update the matrix value
+			config.matrix[rec_row][rec_col] = recv_buffer;					
+		}
+	}
+
+
+	int j; 
+	//Loop through all the inner points of the matrix. The outer points will either be updated by other processes or remain constant.
+	for(i = 1; i < config.local_dims[0] - 1; i++){
+		for(j = 1; j < config.local_dims[1] - 1; j++){
+			//Compute the stepping. Where to define K.
+			config.matrix_tmp[i][j] = config.matrix[i][j] + K * config.d_time * ((config.matrix[i + 1][j] - 2 * config.matrix[i][j] + config.matrix[i - 1][j]) / config.hx / config.hx + (config.matrix[i][j + 1] - 2 * config.matrix[i][j] + config.matrix[i][j -1]) / config.hy / config.hy);
+		}
+	} 
+
+	/*update the config.matrix*/
+	for(i = 1; i < config.local_dims[0] - 1; i++){
+		for(j = 1; j < config.local_dims[0] -1; j++){
+			config.matrix[i][j] = config.matrix_tmp[i][j];
+		}
+	}
+}
+
+
+
+
+
